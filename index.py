ddebc3cc12c7cd9b10c20686296ea15124b1d7fe
ashe <asiasheann@gmail.com>
2022-04-12 01:03:24 +0800

    edit code to clear

--- index.py
+++ index.py
@@ -1,20 +1,14 @@
 import pandas as pd 
-import csv
 import os
 
 
 index = []
 for i in range(0, 10000):
-    if i < 10:
-        index.append("int0000"f"{i}")
-    elif i < 100:
-        index.append("int000"f"{i}")
-    elif i < 1000:
-        index.append("int00"f"{i}")
-    else:
-        index.append("int0"f"{i}")
-
+    s = f"{i}"
+    idx = s.zfill(5)
+    index.append(idx)
 t = {"Int":index}
 df = pd.DataFrame(t)
 os.makedirs("../trait/csv", exist_ok=True)
 df.to_csv("../trait/csv/out_index.csv", index=None)
+
