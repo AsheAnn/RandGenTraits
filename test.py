12e27c477c344ef072881da28698a955b3ccd2c1
ashe <asiasheann@gmail.com>
2022-08-02 16:47:57 +0800

    add relatvie array shuffle function

--- test.py
+++ test.py
@@ -27,20 +27,34 @@ def array2dic(array):
     return result
 
 
-def shuffle_traits():
-    with open("../RandGenTraits/json/traits2.json") as f:
+def relatvie_trait_shuffle():
+    with open("../RandGenTraits/json/traits3.json") as f:
         data = json.load(f)
         traits = data["traits"]
-    new_array = []
+        new_array = []
+        relative_array = []
     for j in range(0, len(traits)):
         count = traits[j]["count"]
         seed = traits[j]["seed"]
         name = traits[j]["name"]
         new_list = []
+        relatvie_list = []
         for i in range(0, count):
-            index = traits[j]["trait"][i]["index"]
-            percent = traits[j]["trait"][i]["percent"]
-            traits_name = traits[j]["trait"][i]["name"]
+            index = traits[j]["type"][i]["index"]
+            percent = traits[j]["type"][i]["percent"]
+            traits_name = traits[j]["type"][i]["name"]
+            color = traits[j]["type"][i]["color"]
+            for k in range(0, len(color)):
+                c_index = color[k]["index"]
+                c_name = color[k]["name"]
+                c_percent = color[k]["percent"]
+                relatvie_list.extend([{
+                    "t_index": index,
+                    "c_index": c_index,
+                }] * int(seed * percent / 100 * (c_percent / 100)))
+            rd.shuffle(relatvie_list)
+            rand_reresult = {f"{name}": relatvie_list}
+            relative_array.append(rand_reresult)
             new_list.extend(([{
                 "index": index,
                 "name": traits_name,
@@ -49,39 +63,29 @@ def shuffle_traits():
             rd.shuffle(new_list)
         rand_result = {f"{name}": new_list}
         new_array.append(rand_result)
-    return array2dic(new_array)
+    return array2dic(relative_array)
+    # return array2dic(new_array)
 
 
 def generate_traits_list(result):
     al = []
     for k in result:
         idz = []
-        scores = []
         tname = []
         for v in range(0, len(result[k])):
-            idz.append(result[k][v]["index"])
-            tname.append(result[k][v]["name"])
-            scores.append(result[k][v]["score"])
+            idz.append(result[k][v]["t_index"])
+            tname.append(result[k][v]["c_index"])
         la = {f"{k}": idz}
-        laa = {"name": tname}
-        laaa = {"score": scores}
-        newa = la | laa | laaa
+        laa = {"body_color": tname}
+        newa = la | laa
         al.append(newa)
     w = []
     for i in range(0, len(al)):
         w.append(pd.DataFrame(al[i]))
-    df = pd.concat(w, axis=1).drop_duplicates(keep="last")
+    df = pd.concat(w, axis=1)
     os.makedirs("./csv", exist_ok=True)
     df.to_csv("./csv/out_4.csv")
 
 
-# def convertToCsv(alist):
-#     df = pd.DataFrame(data=alist).drop_duplicates(keep=False)
-#     os.makedirs("../trait/csv", exist_ok=True)
-#     df.to_csv("../trait/csv/out_1.csv", index=None)
-
 if __name__ == "__main__":
-    # convertToCsv(generate_traits_list(shuffle_traits))
-    # generate_traits_list(shuffle_traits())
-    # print(convertToCsv(generate_traits_list(shuffle_traits())))
-    generate_traits_list(shuffle_traits())
+    generate_traits_list(relatvie_trait_shuffle())
